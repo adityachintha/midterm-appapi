549c4a6cfa29ce98ac11b62e0bfaa3ce414f16aa
Aditya Chintha <adityachintha@gmail.com>
2024-10-18 18:08:22 -0400

    added the try block and if conditions to import users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,4 +8,17 @@ const User = require("../models/userModel");
 const fs = require("fs");
 
 //Function to import the Users from the JSON
-exports.importUsers = async (req, res) => {};
+exports.importUsers = async (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync("./users.json", "utf-8"));
+    const count = await User.countDocuments();
+    if (count === 0) {
+      await User.create(data);
+      console.log("Data imported to MongoDB successfully ");
+      res.status(200).send("Data imported successfully ");
+    } else {
+      console.log("Data already exits");
+      res.status(200).send("Data already exits");
+    }
+  } catch {}
+};
